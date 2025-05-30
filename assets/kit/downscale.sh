66347e71545c10b442e8d60995ab46077032c5dd
Will Shackleton <w.shackleton@gmail.com>
2025-05-30 09:45:31 +0100

    More compressed images

--- assets/kit/downscale.sh
+++ assets/kit/downscale.sh
@@ -2,5 +2,5 @@
 
 for i in original/*
 do
-    convert -resize 1024 -strip -interlace plane $i `echo $i | sed 's,original/,,'` &
+    convert -resize 1024x1024 -strip -interlace plane -quality 80 $i `echo $i | sed 's,original/,,'` &
 done
